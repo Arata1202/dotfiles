5dd34adc48f4245135ed46072845c8691a1ef844
Arata <aratadayoon@yahoo.co.jp>
2025-04-11 23:58:58 +0900

    update: sh

--- .bin/link.sh
+++ .bin/link.sh
@@ -2,20 +2,22 @@ DOTFILES_DIR=~/dotfiles
 
 echo "シンボリックリンクを作成します"
 
-# dotfilesディレクトリ内の二つ下のディレクトリにあるファイルに対してループ
-for file in "$DOTFILES_DIR"/*/.[^.]*; do
-    # '.' と '..' はスキップ
+for file in "$DOTFILES_DIR"/*/* "$DOTFILES_DIR"/*/.[^.]*; do
+
     if [[ $file == "$DOTFILES_DIR/." || $file == "$DOTFILES_DIR/.." ]]; then
         continue
     fi
 
-    # ホームディレクトリ直下に作成するパスを計算
-    link_name=~/${file#"$DOTFILES_DIR"/*/}
+    if [[ $file == "$DOTFILES_DIR/vscode/"* ]]; then
+        target_dir="$HOME/Library/Application Support/Code/User"
+        mkdir -p "$target_dir"
+        link_name="$target_dir/$(basename "$file")"
+    else
+        link_name=~/${file#"$DOTFILES_DIR"/*/}
+    fi
 
-    # シンボリックリンクの作成
     ln -sfn "$file" "$link_name"
 
-    # 作成したシンボリックリンクを出力
     echo "リンクを作成: $file -> $link_name"
 done
 
--- .bin/zsh.sh
+++ .bin/zsh.sh
@@ -1,4 +1,3 @@
-# Brewでzshがインストールされているか確認
 if ! brew list zsh &>/dev/null; then
     echo "zshがインストールされていないため、インストールを開始します..."
     brew install zsh
@@ -6,16 +5,12 @@ else
     echo "zshはすでにインストールされています。"
 fi
 
-# インストールしたzshのパスを取得
 ZSH_PATH=$(brew --prefix)/bin/zsh
 
-# インストールされたzshがシェルとして許可されているか確認し、追加
 if ! grep -Fxq "$ZSH_PATH" /etc/shells; then
     echo "$ZSH_PATH" | sudo tee -a /etc/shells
 fi
 
-# デフォルトシェルをインストールしたzshに変更
 chsh -s "$ZSH_PATH"
 
-# 設定の反映を促すメッセージ
 echo "brewでインストールしたzshをデフォルトシェルに設定しました。ターミナルを再起動してください。"
