2ed57981e4f735768a8ab0f66105983836c39551
Arata <aratadayoon@yahoo.co.jp>
2025-04-14 16:32:39 +0900

    change: code -> code-insiders

--- .bin/code.sh
+++ .bin/code.sh
@@ -1,5 +1,5 @@
 echo "extensionsに記載されている拡張機能をインストールします"
 cat $HOME/dotfiles/vscode/extensions | while read line
 do
-    code --install-extension $line
+    code-insiders --install-extension $line
 done
