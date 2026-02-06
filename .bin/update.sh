81761dfb32215eda93987c74a609b1a6adaf37d7
Arata <aratadayoon@yahoo.co.jp>
2026-02-06 23:01:58 +0900

    fix: update update.sh to include --no-go option and modify .Brewfile for package adjustments

--- .bin/update.sh
+++ .bin/update.sh
@@ -1,5 +1,5 @@
 echo "Updating .Brewfile"
-brew bundle dump --file=homebrew/.Brewfile --force
+brew bundle dump --file=homebrew/.Brewfile --force --no-go
 
 echo "Updating extensions"
 code --list-extensions > vscode/extensions
